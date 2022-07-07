066bd00d0cdcf1d27fc198cfc36fb67ebe4238f8
Joshua James <joshjms1607@gmail.com>
2022-07-07 15:57:01 +0700

    Updates

---
+++ precache-manifest.dff7032185a64fbca189716ef0ac3dc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80ed8330d120ef7c1899",
+    "url": "/minesweeper/static/css/main.744178b8.chunk.css"
+  },
+  {
+    "revision": "80ed8330d120ef7c1899",
+    "url": "/minesweeper/static/js/main.80ed8330.chunk.js"
+  },
+  {
+    "revision": "dea3797aa0b564ef2c8b",
+    "url": "/minesweeper/static/js/1.dea3797a.chunk.js"
+  },
+  {
+    "revision": "e96a486204ff57e2541f",
+    "url": "/minesweeper/static/js/runtime~main.e96a4862.js"
+  },
+  {
+    "revision": "0be6ec2a33949284d1484d51e0f5a002",
+    "url": "/minesweeper/index.html"
+  }
+];
